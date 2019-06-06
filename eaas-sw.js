b1e9c634a1561f2526dd812e7eb10d345dc39667
Rafael Gieschke <rafael@gieschke.de>
2019-06-07 00:38:40 +0200

    Provide eaas-sw.js to external scripts

---
+++ eaas-sw.js
@@ -0,0 +1,21 @@
+// importScripts(`https://emulation-as-a-service.gitlab.io/eaas-proxy/module-loader.js?${new URL(location.search.slice(1), location)}`);
+
+self.window = self;
+
+importScripts("https://emulation-as-a-service.gitlab.io/eaas-proxy/lib/xhr.js");
+// importScripts("https://unpkg.com/browser-es-module-loader@0.4.1/dist/babel-browser-build.js");
+// importScripts("https://unpkg.com/browser-es-module-loader@0.4.1/dist/browser-es-module-loader.js");
+importScripts("https://cdn.jsdelivr.net/gh/rafaelgieschke/browser-es-module-loader@master/dist/babel-browser-build.js");
+importScripts("https://cdn.jsdelivr.net/gh/rafaelgieschke/browser-es-module-loader@master/dist/browser-es-module-loader.js");
+
+new BrowserESModuleLoader().import("http://emulation-as-a-service.gitlab.io/eaas-proxy/@@@/http-request/index.js");
+
+self.oninstall = () => {
+  skipWaiting();
+};
+
+self.onactivate = () => {
+  clients.claim();
+};
+
+self.onfetch = () => {};
