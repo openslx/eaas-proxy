9c18020baa07781f8bdf0b62eb59e3089ec7b305
Rafael Gieschke <rafael@gieschke.de>
2019-06-07 00:28:09 +0200

    http-request: Allow to use EaasClient/WebSocket

--- http-request/index.js
+++ http-request/index.js
@@ -2,13 +2,32 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 import {broadcastStream} from "../lib/broadcast-stream.js";
-import {NIC} from "../webnetwork.js";
+import {NIC, parseMAC} from "../webnetwork.js";
 import {get} from "../lib/idb-keyval.js";
+import EaasClient from "../eaas-client/eaas-client.esm.js";
+import {makeWSStream} from "../lib/websocket-stream.js";
+import {VDEParser, makeVDEGenerator} from "../lib/vde-parser.js";
 
-const nic = new NIC();
+const MAC = parseMAC("ce:8e:83:74:40:2d");
+const nic = new NIC(undefined, MAC);
 (async () => {
+    const useWS = !!await get("apiURL");
+    self.localStorage = {};
+    const client = new EaasClient(await get("apiURL"));
+    client.networkId = await get("networkID");
+    const wsURL = client.wsConnection();
+
+    Object.assign(self, {EaasClient, client, wsURL});
+
+    if (!useWS) {
     const broadcast = broadcastStream("ethernet");
     broadcast.readable.pipeThrough(await nic).pipeThrough(broadcast);
+    } else {
+      (await nic).readable.pipeThrough(makeVDEGenerator())
+      .pipeThrough(makeWSStream(await wsURL))
+      .pipeThrough(new VDEParser())
+      .pipeThrough(await nic);
+    }
     (await nic).addIPv4(await get("clientIP"));
     for (const client of await clients.matchAll()) client.postMessage("");
 })();
@@ -35,6 +54,7 @@ self.requestHttp2 = async ({url}) => {
     const s = new (await nic).TCPSocket(await get("serverIP"), await get("serverPort"));
     console.log(s);
     const w = s.writable.getWriter();
+    url = url.replace(/\/example-exhibit\//, "/");
     w.write(new TextEncoder().encode(`GET ${url}\n`));
     // w.write(new TextEncoder().encode(`GET ${url} HTTP/1.0\r\n\r\n`));
     w.close();
