openslx/eaas-proxy

commit 3288974c08c34a4be8d41c0ffc03b604d40f0ca3

Allow eaas-proxy to connect to an existing network
Usage: ./eaas-proxy.js 127.0.0.1:8080 https://eaas.example/emil/#networkID=3176fcb1-3a2d-4777-9012-093c2adfe92b "" dhcp 10.0.2.15 80

lib/node-eaas-client.js
@@ -16,7 +16,16 @@ const window2 = new JSDOM().window;
 globalThis.jQuery = globalThis.$ = jQueryMod(window2);
 
 export const startEaas = async (url) => {
-    const res = await fetch(url);
+    const urlParsed = new URL(url)
+    const params = new URLSearchParams(urlParsed.hash.slice(1));
+    urlParsed.hash = "";
+    // Allow to connect to an exisiting network via
+    // https://eaas.example/api-url/#networkID=f61f1d5e-59f9-4377-b266-4b4e40a25505
+    if (params.has("networkID")) {
+        const client = new EaasClient(String(urlParsed));
+        client.networkId = params.get("networkID");
+        return client;
+    }
     const url2 = new URL(res.url);
     const config = await (await fetch(new URL("config.json", url2))).json();
     const id = url2.searchParams.get("id");

eaas-proxy.js
@@ -49,8 +49,6 @@ const DEBUG_RECORD_TRAFFIC = process.env.DEBUG_RECORD_TRAFFIC;
   if (params[0].match(/https?:/)) {
     params[0] = await (await startEaas(params[0])).getProxyURL();
   }
-  // HACK: Get rid of `window` for Emscripten to work properly.
-  delete global.window;
 
   if (params[0].startsWith(`${PROTOCOL}:`)) {
     const query = decodeURIComponent(new URL(params[0]).search.slice(1));
@@ -59,6 +57,15 @@ const DEBUG_RECORD_TRAFFIC = process.env.DEBUG_RECORD_TRAFFIC;
       if (v) params[i] = v;
     }
   }
+
+  if (params[1].match(/^https?:\/\//)) {
+    const url = await (await startEaas(params[1])).wsConnection();
+    // Security: wsConnection() MUST not return a socket path!
+    if (!url.match(/^wss?:\/\//)) throw new RangeError(url);
+    params[1] = url;
+  }
+  // HACK: Get rid of `window` for Emscripten to work properly.
+  delete global.window;
 
   const [
     externalIPPortString,

commit 06713356feeca3de4c797e1b9ff3143c075da575

Restore lib/node-eaas-client.js
Broken by commit 3288974c08c34a4be8d41c0ffc03b604d40f0ca3.

lib/node-eaas-client.js
@@ -26,6 +26,7 @@ export const startEaas = async (url) => {
         client.networkId = params.get("networkID");
         return client;
     }
+    const res = await fetch(url);
     const url2 = new URL(res.url);
     const config = await (await fetch(new URL("config.json", url2))).json();
     const id = url2.searchParams.get("id");